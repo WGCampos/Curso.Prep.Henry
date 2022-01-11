74e7df68e086eca91dd6019cc85b85ca5a980e03
WGCampos <wilberguzmanch@gmail.com>
2022-01-11 11:15:04 -0600

    Primer ejercicio funciones

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,7 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  return cadena 
+  return cadena1 
 }
 
 function suma(x, y) {
