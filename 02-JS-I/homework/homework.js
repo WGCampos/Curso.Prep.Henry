d33a94647f59a641db3dfe2923aae6ffcce8bcd7
WGCampos <wilberguzmanch@gmail.com>
2022-01-11 14:41:44 -0600

    Ejercicios Geometria

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -225,21 +225,22 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return (area= alto*ancho);
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  lado=parseInt(prompt("Introduzca un lado de un cuadrado"));
+  return lado *4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  return  ( base  *  altura ) / 2
 }
 
 
@@ -247,7 +248,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  Euros=parseInt(prompt("Cantidad en Euros"));
+  Dollar=Euros*1.20;
 }
 
 
