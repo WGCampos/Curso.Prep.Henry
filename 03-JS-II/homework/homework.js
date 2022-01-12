13d00ae6bb3398eede213e9c92b36d3e34b80668
WGCampos <wilberguzmanch@gmail.com>
2022-01-12 12:50:42 -0600

    funcionObtenerMayor

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,14 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+X=10;
+y=11
+  if (x>y){
+  return x;
+}   
+else {
+  return y;
+}
 }
 
 function mayoriaDeEdad(edad) {
