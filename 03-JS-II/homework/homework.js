6fe293b2ed739a70d62ec0f4b2509e87022839d5
WGCampos <wilberguzmanch@gmail.com>
2022-01-12 16:34:23 -0600

    Uso de OR

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -91,6 +91,12 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero===10 || numero===5){
+    return "True";
+    }
+    else {
+        return "false";
+         }
 }
 
 function estaEnRango(numero) {
