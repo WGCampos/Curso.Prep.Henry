f15eddb2ea38532aba5626d1e1b5972e02625825
WGCampos <wilberguzmanch@gmail.com>
2022-01-12 16:10:10 -0600

    estado de un usuario

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -33,7 +33,15 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-  
+  if (status===1){
+    return "online";
+      }
+      if (status===2){
+        return "Away";
+      }
+      else{
+        return "Offline";
+      }
 }
 
 function saludo(idioma) {
@@ -43,6 +51,7 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  
 }
 
 function colors(color) {
